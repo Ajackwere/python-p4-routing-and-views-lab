1ddb1559ce9c19df72d140379cb3581d2e014038
ajackwere <austinewere59@gmail.com>
2023-09-19 15:59:08 +0300

    Added the function to print numbers

--- server/app.py
+++ server/app.py
@@ -7,6 +7,17 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return '<h1>Python Operations with Flask Routing and Views </h1>'
+@app.route('/print/<string:parameter>')
+def print_string(parameter):
+    print(parameter)
+    return f'Printed string: {paramter}'
+
+@app.route('/count/<int:parameter>')
+def count(parameter):
+    numbers = '\n'.join(str(i) for i in range(1, parameter + 1))
+    return f'Counting numbers from 1 to {parameter}:\n{numbers}'
+
+
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
