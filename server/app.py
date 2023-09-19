f1523baa3d16c8e3624cd80e5029d2e28cc6c22f
ajackwere <austinewere59@gmail.com>
2023-09-19 16:22:57 +0300

    print_string bug removed

--- server/app.py
+++ server/app.py
@@ -4,18 +4,23 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def index():
     return '<h1>Python Operations with Flask Routing and Views </h1>'
+
+
 @app.route('/print/<string:parameter>')
 def print_string(parameter):
-    print(parameter)
-    return f'Printed string: {paramter}'
+    print('parameter')
+    return f'{parameter}'
+
 
 @app.route('/count/<int:parameter>')
 def count(parameter):
     numbers = '\n'.join(str(i) for i in range(1, parameter + 1))
     return f'Counting numbers from 1 to {parameter}:\n{numbers}'
+
 
 @app.route('/math/<float:num1><operation><float:num2>')
 def math(num1, operation, num2):
