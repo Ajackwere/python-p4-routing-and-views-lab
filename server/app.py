0580339ebd099c6f3c95b79b5577e1587583bc06
ajackwere <austinewere59@gmail.com>
2023-09-19 16:12:18 +0300

    Math operation added

--- server/app.py
+++ server/app.py
@@ -18,6 +18,23 @@ def count(parameter):
     return f'Counting numbers from 1 to {parameter}:\n{numbers}'
 
 @app.route('/math/<float:num1><operation><float:num2>')
+def math(num1, operation, num2):
+    if operation == '+':
+        result = num1 + num2
+    elif operation == '-':
+        result = num1 - num2
+    elif operation == '*':
+        result = num1 * num2
+    elif operation == 'div':
+        if num2 != 0:
+            result = num1 / num2
+        else:
+            return 'Division by zero is not possible'
+    elif operation == '%':
+        result = num1 % num2
+    else:
+        return 'Invalid operation'
+    return f'Result of {num1} {operation} {num2} is {result}'
 
 
 if __name__ == '__main__':
